099ac04216445153b3f6a226d7ebd3c4930bf4bd
VlaADanilov <helloampro@gmail.com>
2025-05-02 12:33:37 +0300

    Добавил удаление жалобы

--- src/main/webapp/resources/js/report.js
+++ src/main/webapp/resources/js/report.js
@@ -17,3 +17,33 @@ function going() {
         }
     });
 }
+
+function handleDelete(id) {
+    const isConfirmed = confirm("Вы уверены, что хотите удалить этого сотрудника и лишить его всех прав?");
+    if (isConfirmed) {
+        const url = del.getAttribute("data-url");
+        $.ajax({
+            url: url + "/" + id,
+            type: 'DELETE',
+            success: function (response, status, xhr) {
+                if (xhr.status === 200) {
+                    alert(`Жалоба успешно удалена`);
+                    location.reload(true)
+                }
+            },
+            error: function (xhr, status, error) {
+                try {
+                    const response = JSON.parse(xhr.responseText);
+
+                    if (xhr.status === 404) {
+                        console.log("Report not found");
+                    } else {
+                        console.log("Неизвестная ошибка. Пожалуйста, обратитесь к администратору.");
+                    }
+                } catch (e) {
+                    console.error("Ошибка парсинга JSON:", e);
+                }
+            }
+        });
+    }
+}
